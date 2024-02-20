95464def08cc4d6d8f8ab3a5ae5bc6d0513b2167
sajjad <sajjad.darvish.yektayi@gmail.com>
2024-02-20 14:36:17 +0330

    Create reset action adn password-reset-token data , and update token.ts and mail.ts

--- src/components/auth/reset-form.tsx
+++ src/components/auth/reset-form.tsx
@@ -5,7 +5,7 @@ import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { useState, useTransition } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { LoginSchema } from "@/schemas";
+import { ResetSchema } from "@/schemas";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -19,7 +19,7 @@ import {
 } from "@/components/ui/form";
 
 import { CardWrapper } from ".";
-import { login } from "@/actions/login";
+import { reset } from "@/actions/reset";
 import { FormError, FormSuccess } from "@/components";
 
 export const ResetForm = () => {
@@ -28,21 +28,20 @@ export const ResetForm = () => {
   const [success, setSuccess] = useState<string | undefined>("");
 
   // Create Form type and default Valuesk
-  const form = useForm<z.infer<typeof LoginSchema>>({
-    resolver: zodResolver(LoginSchema),
+  const form = useForm<z.infer<typeof ResetSchema>>({
+    resolver: zodResolver(ResetSchema),
     defaultValues: {
       email: "",
-      password: "",
     },
   });
 
   // Form onSubmit
-  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+  const onSubmit = (values: z.infer<typeof ResetSchema>) => {
     setError("");
     setSuccess("");
 
     startTransition(() => {
-      login(values)
+      reset(values)
         .then((result) => {
           setError(result?.error);
           setSuccess(result?.success);
--- src/lib/tokens.ts
+++ src/lib/tokens.ts
@@ -2,6 +2,7 @@ import { v4 as uuidv4 } from "uuid";
 
 import { db } from "./db";
 import { getVerificationTokenByEmail } from "@/data/verification-token";
+import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4();
@@ -25,6 +26,37 @@ export const deleteVerificationToken = async (email: string) => {
 
   if (existingToken) {
     await db.verificationToken.delete({
+      where: {
+        id: existingToken.id,
+      },
+    });
+  }
+
+  return true;
+};
+
+export const generateResetPasswordToken = async (email: string) => {
+  const token = uuidv4();
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+  await deleteVerificationToken(email);
+
+  const resetPasswordToken = await db.passwordResetToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    },
+  });
+
+  return resetPasswordToken;
+};
+
+export const deleteResetPasswordToken = async (email: string) => {
+  const existingToken = await getPasswordResetTokenByEmail(email);
+
+  if (existingToken) {
+    await db.passwordResetToken.delete({
       where: {
         id: existingToken.id,
       },
